de4aec880ca699c4c833fbca8792e65494f1e543
bertomeu <josecarlosbertomeu@gmail.com>
2024-06-29 17:13:23 +0000

    "Add app.js"
    app.js: add

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generateCard() {
+  const suits = ["spade", "club", "heart", "diamond"];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const symbols = { spade: "♠", club: "♣", heart: "♥", diamond: "♦" };
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+
+  const cardDiv = document.getElementById("card");
+  cardDiv.className = `card ${randomSuit}`;
+
+  const topLeftDiv = cardDiv.querySelector(".top-left");
+  const bottomRightDiv = cardDiv.querySelector(".bottom-right");
+  const centerDiv = cardDiv.querySelector(".center");
+
+  topLeftDiv.innerHTML = symbols[randomSuit];
+  bottomRightDiv.innerHTML = symbols[randomSuit];
+  centerDiv.innerHTML = randomValue;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard();
+  document.getElementById("generateButton").onclick = generateCard;
 };
